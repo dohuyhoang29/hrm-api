c2ae3f776d3267fbb264fb3b7e77e3cb75e64154
HoangDH <hoangdh@vissoft.vn>
2023-02-14 18:34:45 +0700

    add authen

--- src/main/java/com/aptech/hrmapi/security/filter/CustomUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/aptech/hrmapi/security/filter/CustomUsernameAndPasswordAuthenticationFilter.java
@@ -1,8 +1,60 @@
 package com.aptech.hrmapi.security.filter;
 
+import com.aptech.hrmapi.repository.UserRepository;
+import com.aptech.hrmapi.security.utils.SpringContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.springframework.context.ApplicationContext;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class CustomUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private AuthenticationManager authenticationManager;
+
+    public CustomUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+        this.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/admin/verify/**", "POST"));
+    }
+
+    @SneakyThrows
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            UserCredentials credentials = new ObjectMapper().readValue(request.getInputStream(), UserCredentials.class);
+
+            ApplicationContext context = SpringContext.getApplicationContext();
+            UserRepository userRepository = (UserRepository) context.getBean("UserRepository");
+        } catch (Exception e) {
+
+        }
+        return super.attemptAuthentication(request, response);
+    }
+
+    public static class UserCredentials {
+        private String username;
+
+        private String password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
 }
